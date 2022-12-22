4bd7c0ea59a2125e912f11ac6b920b6dc074dcbc
pedrohscarvalho <109320427+pedrohscarvalho@users.noreply.github.com>
2022-12-22 09:20:03 -0300

    Add files via upload

---
+++ javascript-main/src/motos.js
@@ -0,0 +1,41 @@
+let motos = [
+    {
+        id: 1,
+        marca: 'honda',
+        modelo: 'cg 160',
+        ano: '2021', 
+    },
+    {
+        id: 1,
+        marca: 'honda',
+        modelo: 'xre 300',
+        ano: '2023', 
+    },
+    {
+        id: 1,
+        marca: 'yamaha',
+        modelo: 'fazer 250',
+        ano: '2021',
+    },
+    {
+        id: 1,
+        marca: 'bmw',
+        modelo: 'gs 310',
+        ano: '2019',
+    },
+    {
+        id: 1,
+        marca: 'bmw',
+        modelo: 'f800',
+        ano: '2018',
+    },
+    {
+        id: 1,
+        marca: 'triumph',
+        modelo: 'tiger 800',
+        ano: '2022',
+    }
+
+
+
+]
